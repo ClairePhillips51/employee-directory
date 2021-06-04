a3cf03c0ca478ff174499b6bc885b1e68ec7b99c
Claire Phillips <claireacam@gmail.com>
2021-06-04 18:16:19 -0400

    set up the employee index

---
+++ src/components/Employee/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Employee(props) {
+    const employee = props.employee;
+    return (
+        <tr>
+            <td>
+                <img src={employee.picture.thumbnail}/>
+            </td>
+            <td>{employee.name.first} {employee.name.last}</td>
+            <td>{employee.email}</td>
+            <td>{employee.phone}</td>
+            <td>{employee.cell}</td>
+        </tr>
+    );
+}
+
+export default Employee;
