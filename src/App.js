15f274ea815df5483a2a268f5715bba8854e85bf
Claire Phillips <claireacam@gmail.com>
2021-06-04 19:10:00 -0400

    setup filter.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import API from "./utils/API";
-import React, { useState } from 'react';
-//import SearchBar from "./components/SearchBar";
+import React, { useState, useEffect } from 'react';
+import SearchBar from "./components/SearchBar";
 import Table from "./components/Table";
 
 function App() {
   const [employees, setEmployees] = useState([]);
-  API.getRandomEmployees().then(res => {
-    setEmployees(res.data.results);
-    console.log(employees);
-  });
+  const [filterEmployees, setFilterEmployees] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    API.getRandomEmployees().then(res => {
+      setEmployees(res.data.results);
+      setFilterEmployees(res.data.results);
+    });
+  }, []);
+
+  useEffect(() => {
+    const filtered = employees.filter(e => (e.name.first.toUpperCase()+" "+e.name.last.toUpperCase()).includes(filter.toUpperCase()));
+    setFilterEmployees(filtered);
+  }, [filter, employees]);
+
   return (
     <div className="App">
-     <Table
-      employees = {employees}
-     /> 
+      <SearchBar
+        setFilter = {setFilter}
+      />
+      <Table
+        employees = {filterEmployees}
+      /> 
     </div>
   );
 }
